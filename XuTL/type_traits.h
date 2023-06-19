371c74db959f147fcb725228c8265275c8385fd1
xxxujie <604365090@qq.com>
2023-06-19 21:37:10 +0800

    增加了 integral_constant 和 true_type、false_type

--- XuTL/type_traits.h
+++ XuTL/type_traits.h
@@ -2,6 +2,23 @@
 #define XUTL_TYPE_TRAITS_H_
 
 namespace xutl {
+
+// helper class:
+
+template <typename T, T v>
+struct integral_constant {
+    static constexpr const T value = v;
+    using value_type = T;
+    using type = integral_constant;
+    // 转型函数
+    inline constexpr operator value_type() const noexcept {
+        return value;
+    }
+};
+
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
 // enable_if
 template <bool, typename T = void>
 struct enable_if {};
